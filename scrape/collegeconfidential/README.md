2d9d2d6205676938b00f4d472c7bf998a3e577a0
David Wihl <wihl@ripariandata.com>
2015-10-31 15:26:53 -0400

    following next search page; maxthreads added

--- scrape/collegeconfidential/README.md
+++ scrape/collegeconfidential/README.md
@@ -1,6 +1,6 @@
 ### Scrape College Confidential ###
 
-`scrape.py` performs a download / scrape of College Confidential. It performs a search for "actual results" and then walks the messages to output in JSONm called `messages.json`.
+`scrape.py` performs a download / scrape of College Confidential. It performs a search for "actual results" and then walks the messages to output in a JSON-encoded file called `messages.json`.
 
 Run it simply from the command line:
 
@@ -10,6 +10,9 @@ python scrape.py
 It will print a line for every thread it finds and then
 print a summary of total threads written to JSON at the end.
 
+The current version stops at 1000 messages which should
+be sufficient to build the parsers.
+
 ### JSON File Format ###
 
 The JSON file is a single large array of threads:
@@ -17,9 +20,11 @@ The JSON file is a single large array of threads:
 ```json
 [
 	{ "date" : date-str,
+	  "url" : url,
 	  "messages" : message-body
 	},
 	{ "date", date-str,
+	  "url" : url,
 	  "messages" : message-body
 	},
 	...
@@ -28,6 +33,7 @@ The JSON file is a single large array of threads:
 where
 
 * `date-str` is the UTC encoded date of the first message in the thread.
+* `url` is the source URL of the thread
 * `message-body` is the content of all the text in the thread. Separate messages in the thread are concatenated into a single long block of text. No header information is preserved.
 
 #### Viewing the Downloaded JSON ###
--- scrape/collegeconfidential/scrape.py
+++ scrape/collegeconfidential/scrape.py
@@ -21,7 +21,7 @@ def read_thread(url):
     # extract time of posting
     itemHeader = soup.find("div", attrs={"class":"DiscussionHeader"})
     firstTime = itemHeader.find("time")['datetime']
-    thread = { 'date': firstTime, 'messages':"" }
+    thread = { 'date': firstTime, 'url':url, 'messages':"" }
 
     #extract msgs
     for msg in soup.find("div",attrs={"class":"Message"}):
@@ -32,18 +32,30 @@ def read_thread(url):
 
 def search_msgs():
     # search for all messages with "actual results" in the message
-    req = requests.get("http://talk.collegeconfidential.com/search?adv=1&search=actual+results&title=&author=&cat=all&tags=&discussion_d=1&within=1+day&date=")
+    baseurl = "http://talk.collegeconfidential.com"
+    req = requests.get(baseurl + "/search?adv=1&search=actual+results&title=&author=&cat=all&tags=&discussion_d=1&within=1+day&date=")
+    maxthreads = 1000
+    results = []
+
+    while (maxthreads > 0 and req.status_code == 200):
+        soup = BeautifulSoup(req.text,"lxml")
+        for item in soup.findAll("li", attrs={"class","Item-Search"}):
+            url = item.findAll("h3")[0].find("a",href=True)['href']
+            results.append(read_thread(url))
+            maxthreads -= 1
+            time.sleep(1)
+        
+        # get the next page of results
+        nextpage = soup.find("a", attrs={"class","Next"},href=True)
+        if not nextpage: break
+        req = requests.get(baseurl + nextpage['href'])
 
     if req.status_code != 200:
         print "Could not search, Err=",req.status_code
         return
+    else:
+        print "Max threads reached or no additional threads found."
 
-    results = []
-    soup = BeautifulSoup(req.text,"lxml")
-    for item in soup.findAll("li", attrs={"class","Item-Search"}):
-        url = item.findAll("h3")[0].find("a",href=True)['href']
-        results.append(read_thread(url))
-        time.sleep(1)
 
     write_json(results)
         
